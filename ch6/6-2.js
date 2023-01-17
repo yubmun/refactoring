0df95fb935d05a9b0478aa651d0042487cdcb6fb
yubmun <dlwnsduq23@gmail.com>
2023-01-17 11:07:54 +0900

    6-2 리팩토링 진행

--- ch6/6-2.js
+++ ch6/6-2.js
@@ -1,20 +1,14 @@
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 // 예제 2
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
 }
 
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
-}
+// 너무 기능을 쪼갠 함수는 가독성에 좋지 않다. 합칠건 합치자
