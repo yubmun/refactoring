139afcab7c0bb3997c7c6016ac5e15265fa9759f
yubmun <dlwnsduq23@gmail.com>
2023-01-18 11:13:48 +0900

    복습시 꼭 봐야될 커밋 class, getter, private활용

--- ch6/6-9-client2.js
+++ ch6/6-9-client2.js
@@ -1,10 +1,7 @@
 import { acquireReading, baseRate } from './6-9.js';
 
-const aReading = acquireReading();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const reading = acquireReading();
 
-function taxThreshold(year) {
-  return 0.1;
-}
+export const taxableCharge = reading.taxableCharge;
 
-export const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+console.log(taxableCharge);
