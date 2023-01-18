fe838ca3dc8cf8a124f102c8dfccd00a9e223058
yubmun <dlwnsduq23@gmail.com>
2023-01-18 10:07:46 +0900

    6-5-1, 6-5-2, 6-5-3 리팩토링

--- ch6/6-5-2.js
+++ ch6/6-5-2.js
@@ -1,10 +1,18 @@
+// 함수 선언 바꾸기
+// VIP 예약 우선권은 구현하고 싶다고 가정하자.
 export default class Book {
   #reservations;
   constructor() {
     this.#reservations = [];
   }
 
-  addReservation(customer) {
+  // flag (true, false에 따라 다른 동작을 하게 만드는 boolean을 칭함)를 이용하여
+  // 함수의 동작사항을 변경하는 것은 좋지 않습니다.
+  // 이에 대해서는 추후에 조금 더 자세히 다룰 예정입니다.
+
+  // 정말 필요한 flag 라고 가정한다면 아래와 같이 구현할 수 있습니다.
+  // 일반 예약을 기준으로 default 값으로 false를 지정하여 줍니다.
+  addReservation(customer, isPriority = false) {
     this.#reservations.push(customer);
   }
 
--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,8 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+// 함수 선언 바꾸기
+
+// 결국 함수에서 필요한 것은 aCustomer가 아니라
+// state이다!
+// 따라서 state만 받아오면 되는 것이다!
+export function inNewEngland(state) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(state);
 }
