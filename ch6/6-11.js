bea6341bc689e40809c77dc1e3f5a00d5ca79373
yubmun <dlwnsduq23@gmail.com>
2023-01-19 11:14:40 +0900

    함수 쪼개기 실습

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,16 +1,33 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
+  // 기본 제품 가격을 계산
+  const basePrice = calculateBasePrice(product, quantity);
+
+  // 할인가격 계산
+  const discount = calculateDiscount(product, quantity);
+
+  // 총 배송비 계산
+  const shippingCost = calculateShippingCost(basePrice, quantity, shippingMethod);
+
+  // 총 가격
+  return basePrice - discount + shippingCost;
+}
+
+function calculateBasePrice(product, quantity){
+  return product.basePrice * quantity;
+}
+
+function calculateDiscount(product, quantity) {
+  return (
+    Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate
+    );
+}
+function calculateShippingCost(basePrice, quantity, shippingMethod){
   const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  basePrice > shippingMethod.discountThreshold
+    ? shippingMethod.discountedFee
+    : shippingMethod.feePerCase;
+
+  return quantity * shippingPerCase;
 }
 
 // 사용 예:
