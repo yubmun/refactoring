56c3f0b6a52f4a6bd9bbb265c72a44f6430f4e05
yubmun <dlwnsduq23@gmail.com>
2023-01-25 10:15:13 +0900

    함수 쪼개기

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,6 +2,36 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+  isHighPriority(){
+    return 'high' === this.priority || 'rush' === this.priority;
+  }
+}
+
+class Priority{
+  #value;
+  constructor(value){
+    if(Priority.legalValues().includes(value)){
+      this.#value = value;
+    } else {
+      throw new Error(`${value} 는 포함되지 않는 범주입니다.`); // 생성자 안에서 에러를 던지는건 보안에 취약한 코드라 추천하지는 않는다
+    }
+  }
+
+  get index(){
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  equals(other){
+    return this.index === other.#index;
+  }
+
+  higherThan(other){
+    return this.index > other.index;
+  }
+
+  static legalValues(){
+    return ["low", "normal", "high", "rush"]
+  }
 }
 
 const orders = [
@@ -11,5 +41,4 @@ const orders = [
 ];
 
 const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+  (o) => o.isHighPriority()).length;
