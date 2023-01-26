59c0bffd81021511c10e888ef266f1fe9b991f3c
yubmun <dlwnsduq23@gmail.com>
2023-01-26 10:22:34 +0900

    if문 삼항연산자로 변경, 문장 슬라이스

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,18 +1,12 @@
 // 예제 1
 const pricingPlan = retrievePricingPlan();
+const chargePerUnit = pricingPlan.unit;
+// 관련있는 코드를 묶어주는게 가독성에 좋다
 const order = retreiveOrder();
 let charge;
-const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result = availableResources.length === 0 ? createResource() : allocatedResources.pop();
   return result;
 }
