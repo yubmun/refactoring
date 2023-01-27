2aae8981dfe823082ece20c52784c5077e18beb8
yubmun <dlwnsduq23@gmail.com>
2023-01-27 11:11:40 +0900

    조건식 통합하기

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,15 @@
+// 조건식 통합하기
+
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  if (isNotEligibleForDisability(employee)){
+    return 0;
+  }
+
   return 1;
+}
+
+function isNotEligibleForDisability(employee){
+  return(
+    employee.seniority < 2 || employee.monthsDisabled > 12 || employee.isPartTime
+  )
 }
