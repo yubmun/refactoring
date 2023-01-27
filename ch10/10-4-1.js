80bbb8fdfac513cde1533aa01ee669f0162af5c3
yubmun <dlwnsduq23@gmail.com>
2023-01-27 11:40:04 +0900

    객체지향적으로 추상클래스

--- ch10/10-4-1.js
+++ ch10/10-4-1.js
@@ -1,11 +1,63 @@
 export function plumages(birds) {
-  let map = birds.map((b) => [b.name, plumage(b)]);
+  let map = birds.map((b) => [b.name, b.plumage]);
   let map1 = new Map(map);
   return map1;
 }
 export function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+  return new Map(birds.map((b) => [b.name, b.airSpeedVelocity]));
 }
+
+class Bird{
+  #name;
+  constructor(name){
+    this.#name = name;
+  }
+  get name (){
+    return this.#name;
+  }
+
+  get plumage(){
+    return "unknown";
+  }
+  get airSpeedVelocity(){
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird{
+  constructor(){
+    super("EuropeanSwallow");
+  }
+  get plumage(){
+    return "average";
+  }
+  get airSpeedVelocity(){
+    return 35;
+  }
+}
+class AfricanSwallow extends Bird {
+  constructor(){
+    super("AfricanSwallow");
+  }
+  get plumage(){
+    return this.numberOfCoconuts > 2 ? "tired" : "average";
+  }
+  get airSpeedVelocity(){
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+class NorwegianBlueParrot extends Bird {
+  constructor(){
+    super("NorwegianBlueParrot");
+  }
+  get plumage(){
+    return this.voltage > 100 ? "scorched" : "beautiful";
+  }
+  get airSpeedVelocity(){
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
+
 export function plumage(bird) {
   switch (bird.type) {
     case 'EuropeanSwallow':
@@ -30,3 +82,6 @@ export function airSpeedVelocity(bird) {
       return null;
   }
 }
+
+const result = plumages([new NorwegianBlueParrot(), new AfricanSwallow()]);
+console.log(result);
