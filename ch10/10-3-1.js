c441352ffd9eae484c7d55b9ea0713f1f296e0a0
yubmun <dlwnsduq23@gmail.com>
2023-01-27 11:22:36 +0900

    조건식 쪼개기

--- ch10/10-3-1.js
+++ ch10/10-3-1.js
@@ -1,20 +1,17 @@
 export function payAmount(employee) {
-  let result;
+  
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return { amount: 0, reasonCode: 'SEP' };
   }
-  return result;
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: 'RET' };
+  }
+    // lorem.ipsum(dolor.sitAmet);
+    // consectetur(adipiscing).elit();
+    // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    // ut.enim.ad(minim.veniam);
+
+  return someFinalComputation();
 }
 
 function someFinalComputation() {
