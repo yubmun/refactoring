b4278ebb4a90d30412d4afda3f31a6e2ee288b2a
yubmun <dlwnsduq23@gmail.com>
2023-02-01 10:16:29 +0900

    break 제어플래그 활용해서 조건문 없애기

--- ch10/10-7.js
+++ ch10/10-7.js
@@ -1,8 +1,8 @@
+// 제어플래그를 탈출문으로 바꾸기
+
 for (const p of people) {
-  if (!found) {
-    if (p === 'Don') {
-      sendAlert();
-      found = true;
-    }
+  if (p === 'Don') {
+    sendAlert();
+    break;
   }
 }
