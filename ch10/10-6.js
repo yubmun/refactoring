e14a390b646cae1af7af9ec92f52012f2f8364e6
yubmun <dlwnsduq23@gmail.com>
2023-02-01 10:11:39 +0900

    assertion 추가

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,15 @@
+// Assertion 추가하기
+import { strict as assert } from "node:assert";
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    assert(number >= 0);
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+new Customer().applyDiscount(1);
+
