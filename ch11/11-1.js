c0a7500898a6806ddeeb8d3d8cbec245da890982
yubmun <dlwnsduq23@gmail.com>
2023-02-01 10:31:17 +0900

    side effect가 없는 함수로 리팩토링

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,26 +1,31 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
+// side effect 없는 함수 만들기를 목표로 하고 코드를 짜야한다. 함수는 한가지 일을 수행해야 한다.
+function getTotalOutstanding() {
   const result = customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
-  sendBill();
   return result;
+}
+function sendBill(){
+  // sendBill 해주는 함수 작성
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant(people){
   for (const p of people) {
     if (p === 'Don') {
-      setOffAlarms(alarm, p);
       return 'Don';
     }
     if (p === 'John') {
-      setOffAlarms(alarm, p);
       return 'John';
     }
   }
-  return '';
 }
 
 function setOffAlarms(alarm, p) {
