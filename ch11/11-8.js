befbe293191fc409f0f9588a5b6a7978faab3013
yubmun <dlwnsduq23@gmail.com>
2023-02-02 10:34:57 +0900

    팩토리 함수는 언제쓰이나

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,4 +1,5 @@
 export class Employee {
+  // 생성자는 캡슐화했다고 가정
   constructor(name, typeCode) {
     this._name = name;
     this._typeCode = typeCode;
@@ -14,4 +15,10 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+  // 팩토리 함수는 외부에서 손쉽고 간결하게 사용할 수 있도록 하기 위해서 사용한다.
+  static createEnginner(name){
+    return new Employee(name, "E")
+  }
 }
+
+const employee = Employee.createEnginner("준엽");
