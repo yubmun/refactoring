8a68e1613fbe60cb1c8d589ac8d2e952b702479e
yubmun <dlwnsduq23@gmail.com>
2023-02-03 10:36:20 +0900

    생성자에서 에러를 던지는 코드는 좋지 않다

--- ch12/12-6.js
+++ ch12/12-6.js
@@ -1,26 +1,52 @@
 class Employee {
   #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
+  constructor(name) {
     this.#name = name;
-    this.#type = type;
   }
 
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
-  }
-
-  get type() {
-    return this.#type;
+  get type(){
+    return "employee";
   }
 
   toString() {
     return `${this.#name} (${this.type})`;
   }
+
+  // static validateType(arg) {
+  //   if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
+  //     throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+  //   }
+  // }
+  // 생성자에서 에러를 던지는건 나쁜 코드다
+
+  static createEmployee(name, type){
+    switch(type){
+      case "engineer":
+        return new Engineer(name, type);
+      case "salesperson":
+        return new Salesperson(name, type);
+      case "manager":
+        return new Manager(name, type);
+      default :
+        throw new Error(`${tpye}라는 직원 유형은 없습니다.`);
+    }
+  }
+}
+class Engineer extends Employee{
+  get type(){
+    return "engineer"
+  }
+}
+class Manager extends Employee{
+  get type(){
+    return "manager";
+  }
+}
+class Salesperson extends Employee{
+  get type(){
+    return "salesperson";
+  }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+const ellie = new Engineer('엘리', 'engineer');
+const bob = new Manager('밥', 'manager');
