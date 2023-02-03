353fe00c0b5e18776a9a111c65cb714ec050ccfe
yubmun <dlwnsduq23@gmail.com>
2023-02-03 10:26:34 +0900

    필드값 내리기기

--- ch12/12-5.js
+++ ch12/12-5.js
@@ -1,6 +1,8 @@
 class Employee {
-  #quota;
+  // 모든 자식클래스가 갖지 않는 필드값을 부모클래스에서 쓰지는 않는다.
 }
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  #quota;
+}
