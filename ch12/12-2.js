48d9e88d7f0c77a9d573e7e08e8c0f00597d5b18
yubmun <dlwnsduq23@gmail.com>
2023-02-03 10:18:59 +0900

    부모클래스로 필드옮기기

--- ch12/12-2.js
+++ ch12/12-2.js
@@ -1,9 +1,11 @@
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   #name;
 }
 
+class Salesperson extends Employee {
+  
+}
+
 class Engineer extends Employee {
-  #name;
+
 }
