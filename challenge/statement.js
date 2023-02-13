cf25c1f7c1fb070d85316aebdea3e263860d8dae
yubmun <dlwnsduq23@gmail.com>
2023-02-13 12:30:10 +0900

    로우레벨 리팩토링

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,25 +1,41 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays){
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+    // 청구 내역을 출력한다.
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(totalAmount() / 100)}\n`;
+  result += `적립 포인트: ${totalCredits()}점\n`;
+  return result;
 
-    switch (play.type) {
-      case 'tragedy': // 비극
+  function playFor(perf) {
+    return plays[perf.playID];
+  }
+
+  function creditsFor(perf) {
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) {
+      result += Math.floor(perf.audience / 5);
+    }
+    return result;
+  }
+
+  function amountFor(perf) {
+    let thisAmount = 0;
+    switch (playFor(perf).type) {
+      case "tragedy": // 비극
         thisAmount = 40000;
         if (perf.audience > 30) {
           thisAmount += 1000 * (perf.audience - 30);
         }
         break;
-      case 'comedy': // 희극
+      case "comedy": // 희극
         thisAmount = 30000;
         if (perf.audience > 20) {
           thisAmount += 10000 + 500 * (perf.audience - 20);
@@ -27,46 +43,49 @@ export function statement(invoice, plays) {
         thisAmount += 300 * perf.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(perf).type}`);
     }
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
+
+  function totalAmount() {
+    return invoice.performances.reduce((sum, p) => (sum += amountFor(p)), 0);
+  }
+
+  function totalCredits() {
+    return invoice.performances.reduce((sum, p) => (sum += creditsFor(p)), 0);
+  }
+}
+
+function usd(number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(number);
 }
 
 // 사용예:
 const playsJSON = {
-  hamlet: { name: 'Hamlet', type: 'tragedy' },
-  'as-like': { name: 'As You Like It', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
 };
 
 const invoicesJSON = [
   {
-    customer: 'BigCo',
+    customer: "BigCo",
     performances: [
       {
-        playID: 'hamlet',
+        playID: "hamlet",
         audience: 55,
       },
       {
-        playID: 'as-like',
+        playID: "as-like",
         audience: 35,
       },
       {
-        playID: 'othello',
+        playID: "othello",
         audience: 40,
       },
     ],
@@ -75,11 +94,11 @@ const invoicesJSON = [
 
 const result = statement(invoicesJSON[0], playsJSON);
 const expected =
-  '청구 내역 (고객명: BigCo)\n' +
-  '  Hamlet: $650.00 (55석)\n' +
-  '  As You Like It: $580.00 (35석)\n' +
-  '  Othello: $500.00 (40석)\n' +
-  '총액: $1,730.00\n' +
-  '적립 포인트: 47점\n';
+  "청구 내역 (고객명: BigCo)\n" +
+  "  Hamlet: $650.00 (55석)\n" +
+  "  As You Like It: $580.00 (35석)\n" +
+  "  Othello: $500.00 (40석)\n" +
+  "총액: $1,730.00\n" +
+  "적립 포인트: 47점\n";
 console.log(result);
 console.log(result === expected);
